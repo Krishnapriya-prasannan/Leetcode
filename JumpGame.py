559b22263722e8d49802e40006f87475d4d3d8eb
Krishnapriya Prasannan <117696865+Krishnapriya-prasannan@users.noreply.github.com>
2025-05-29 10:30:37 +0530

    Create JumpGame.py

---
+++ JumpGame.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxIndex=0
+        for i in range(len(nums)):
+            if i>maxIndex:
+                return False
+            maxIndex=max(maxIndex,i+nums[i])
+
+        return True
