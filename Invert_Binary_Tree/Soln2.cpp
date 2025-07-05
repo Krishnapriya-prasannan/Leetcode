636e747faa188cfeda05b830babebd9682a5be35
Krishnapriya Prasannan <117696865+Krishnapriya-prasannan@users.noreply.github.com>
2025-07-05 06:08:11 +0530

    Create Soln2.cpp

---
+++ Invert_Binary_Tree/Soln2.cpp
@@ -0,0 +1,27 @@
+/**
+recursive dfs approach
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr){
+            return root;
+        }
+        TreeNode* leftsubtree=invertTree(root->left);
+        TreeNode* rightsubtree=invertTree(root->right);
+
+        root->left=rightsubtree;
+        root->right=leftsubtree;
+        
+        return root;
+    }
+};
